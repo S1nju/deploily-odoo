fee7fb070f6d090c34566def71ae74169a81cb21
benadda fatima <fatima.benadda@transformatek.dz>
2026-06-04 15:17:09 +0100

    feat: Add Messenger & Instagram integration with webhook handling and CRM lead conversion

---
+++ src/messenger_crm_lead/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Social Inbox: Messenger & Instagram',
+    'version': '18.0.1.0.0',
+    'category': 'Sales/CRM',
+    'summary': 'Receive Facebook Messenger & Instagram DM and convert to CRM Leads with 1 click.',
+    'description': """
+        Features:
+        - Webhook for Facebook Messenger & Instagram DM
+        - Incoming messages stored in Odoo inbox
+        - 1-Click Convert to Lead button
+        - Auto-create lead option
+        - Settings for Page Access Token & Verify Token
+    """,
+    'author': 'Saydun Nobi',
+    'website': '',
+    'depends': ['crm', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/messenger_message_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/menu.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
