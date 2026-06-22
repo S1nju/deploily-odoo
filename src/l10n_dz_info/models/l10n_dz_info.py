fb44512410777e1d1a7a1511623e8f417db5128c
benadda fatima <fatima.benadda@transformatek.dz>
2026-06-22 11:33:54 +0100

    refactor: Comment out unused ResPartner model and related XML view

--- src/l10n_dz_info/models/l10n_dz_info.py
+++ src/l10n_dz_info/models/l10n_dz_info.py
@@ -30,17 +30,17 @@ class ResCompany(models.Model):
     )
 
 
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
+# class ResPartner(models.Model):
+#     _inherit = 'res.partner'
 
-    rc = fields.Char(string='R.C')
-    nif = fields.Char(string='N.I.F', size=15)
-    nis = fields.Char(string='N.I.S')
-    ai = fields.Char(string='Article d\'imposition')
-    commune_id = fields.Many2one(
-        'res.country.state.commune',
-        string='Commune',
-        domain="[('state_id', '=', state_id)]",
-    )
+#     rc = fields.Char(string='R.C')
+#     nif = fields.Char(string='N.I.F', size=15)
+#     nis = fields.Char(string='N.I.S')
+#     ai = fields.Char(string='Article d\'imposition')
+#     commune_id = fields.Many2one(
+#         'res.country.state.commune',
+#         string='Commune',
+#         domain="[('state_id', '=', state_id)]",
+#     )
 
 
